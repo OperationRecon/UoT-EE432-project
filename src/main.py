74e0ae7b7bec2e90ea013e37c3df2dd63728fc79
Mohammad <mohammadnaas203@gmail.com>
2024-09-05 09:47:50 +0200

    Added a prototype to what the main function of the code should be. as well as a proposition to the way user-input commands are to be processed.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,37 @@
-print('Hello World!')
+# Function to be used in the CLI. most likely to be moved to a seperate file/folder. 
+def hello_message():
+    print('Hello World')
+
+def login():
+    u = input('username: ')
+    p = input('password: ')
+
+    if u == 'admin' and p == '12345678':
+        print('welcome, Admin!')
+    else:
+        print('Invalid Credintials')
+
+def commands_help(command = ''):
+    command_list = ['hello','login','help']
+    if command not in commands:
+        for i in command_list:
+            print(i)
+
+# Dict containing the CLI commands related to each function. 
+# Typing the key string in the terminal executes the value command
+commands = {'hello': hello_message, 'login':login, 'help':commands_help}
+
+# main file, to include initialisations and the CLI responses.
+def main():
+    print('Welcome to admin system. Enter help to open command list.')
+    while True:
+        command = input()
+        if command in commands:
+            commands[command]()
+        elif(command == 'exit'):
+            break   
+        else:
+            print('Uknown command. spell-check or Enter help to open command list.')
+
+if __name__ == '__main__':
+    main()
