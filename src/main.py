fa6e29ea1bd5b7d87d831921870832837526247f
Has28 <has2809@hotmail.com>
2024-09-06 11:43:37 +0200

    Edited the structure

--- src/main.py
+++ src/main.py
@@ -1,40 +1,9 @@
-# Function to be used in the CLI. most likely to be moved to a seperate file/folder. 
-def hello_message():
-    print('Hello World')
+from database import init_db
+from cli.menu import run_cli
 
-
-def login():
-    u = input('username: ')
-    p = input('password: ')
-
-    if u == 'admin' and p == '12345678':
-        print('welcome, Admin!')
-    else:
-        print('Invalid Credintials')
-
-
-def commands_help(command = ''):
-    command_list = ['hello','login','help']
-    if command not in commands:
-        for i in command_list:
-            print(i)
-
-# Dict containing the CLI commands related to each function. 
-# Typing the key string in the terminal executes the value command
-commands = {'hello': hello_message, 'login':login, 'help':commands_help}
-
-# main file, to include initialisations and the CLI responses.
 def main():
-    print('Welcome to admin system. Enter help to open command list.')
-    while True:
-        command = input()
-        if command in commands:
-            commands[command]()
-        elif(command == 'exit'):
-            break   
-        else:
-            print('Uknown command. spell-check or Enter help to open command list.')
+    init_db()
+    run_cli()
 
-
-if __name__ == '__main__':
+if name == "main":
     main()
