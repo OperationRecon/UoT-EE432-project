eeecd767a708b73547baf91e9db8bb5d4592c531
Has28 <has2809@hotmail.com>
2024-09-07 03:38:15 +0200

    Edited menu formatting

--- src/CLI/menu.py
+++ src/CLI/menu.py
@@ -3,8 +3,12 @@ from commands.user_management import *
 from models.user import User
 from services import user_service, subject_service, enrollment_service, grade_service
 
+
 def command_help(user):
     for i in commands.keys():
+        if i == "login":
+            print(i, " (With a different user)")
+            continue
         print(i)
 
 
@@ -12,10 +16,10 @@ def display_main_menu():
     print("\nUniversity Administration System")
     print("Login by entering 'login' or close using 'exit'.")
 
+
 def display_user_menu(user):
     print(f"\nWelcome, {user.name}")
     print("Use 'help' to get a list of available commands.")
-
 
 
 def handle_user_input():
@@ -26,25 +30,27 @@ def handle_user_input():
         if not current_user:
             display_main_menu()
 
-        command = input()
-        if command and command not in commands:
+        command = input(">>")
+        if command and command in commands:
             if command == "exit":
                 break
 
-            if command == 'login': 
+            elif command == 'login':
                 current_user = login()
                 if current_user:
                     display_user_menu(current_user)
-
                 continue
 
-            print("Invalid command!")
+            else:
+                commands[command](current_user)
 
         else:
-            commands[command](current_user)
+            print("Invalid command!")
+
 
 def run_cli():
     handle_user_input()
 
+
 # List of commands available to all users
-commands = {"help":command_help, "add subject":add_subject}
+commands = {"login": "", "help": command_help, "exit": "", "add subject": add_subject}
