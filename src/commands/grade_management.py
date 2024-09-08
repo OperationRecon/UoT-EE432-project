c3a5941976ff2a5cbbfe2d29ac766e36348c73e3
Mohammad <mohammadnaas203@gmail.com>
2024-09-08 21:23:46 +0200

    added add grade admin command

--- src/commands/grade_management.py
+++ src/commands/grade_management.py
@@ -1,12 +1,28 @@
 import utils.helpers
+from services import grade_service
 from models.admin import Admin
 from models.teacher import Teacher
 from models.student import Student
 
+
 def add_grade(user):
     if not utils.helpers.verify_role(type(user), [Admin]):
         return
-    pass
+    subject = input("Enter subject code: ")
+    student = input("Enter student ID: ")
+    sem = input("Enter semester: ")
+    yearwork = input("Enter yearwork grades: ")
+    final = input("Enter Final Grade: ")
+
+    
+    try:
+        grade_service.add_grade((subject,student,sem,yearwork,final))
+        print("Grade Added successully!")
+
+    except Exception as e:
+        print(f"Error adding grade: {e}")
+    
+
 
 
 def get_grade(user):
