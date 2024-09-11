9bc6be29d79c399f669d7a3993f36a57de82dcc3
ahmedbarka8 <Ahmed105barka@gmail.com>
2024-09-11 03:58:44 +0200

    Added user_id and subject_code validations

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,16 +1,40 @@
 from services import grade_service, user_service, subject_group_service, subject_service
 
 
-def validate_user_data(user_id):
-    if not user_service.get_user(user_id):
-        print(f"User with id: {user_id} doesn't exist")
-        return
+def validate_student_data(user_id):
+    user = user_service.get_user(user_id)
+    while not user or user.user_type != "student":
+        print(f"Student with id: {user_id} doesn't exist.")
+        user_id = input("Enter another student ID or exit with 'exit': ")
+        if user_id == "exit":
+            return None
+        user = user_service.get_user(user_id)
+        continue
+    return user_id
+
+def validate_teacher_data(user_id):
+    user = user_service.get_user(user_id)
+    while not user or user.user_type != "teacher":
+        print(f"Student with id: {user_id} doesn't exist.")
+        user_id = input("Enter another teacher ID or exit with 'exit': ")
+        if user_id == "exit":
+            return None
+        user = user_service.get_user(user_id)
+        continue
+    return user_id
+
 
 
 def validate_subject(subject_code):
-    if not user_service.get_user(subject_code):
-        print(f"Subject with code: {subject_code} doesn't exist")
-        return
+    subject = subject_service.get_subject(subject_code)
+    while not subject:
+        print(f"Subject with code: {subject_code} doesn't exist.")
+        subject_code = input("Enter another subject or exit with 'exit': ")
+        if subject_code == "exit":
+            return None
+        subject = subject_service.get_subject(subject_code)
+        continue
+    return subject_code
 
 
 def validate_subject_group(grade_data):
